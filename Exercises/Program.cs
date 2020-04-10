523a557741de8dc6e50b36a853f3de2f28be4cd6
Dawson <vulcangamelabs@gmail.com>
2020-04-10 00:17:16 -0500

    Completed more exercises from 5 - 8

--- Exercises/Program.cs
+++ Exercises/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 /*
 =============================================================================================
@@ -258,63 +259,250 @@ namespace Exercises
             // Sample input for operation: 1
             // Expected output: 3
 
-            string lineSpam = new string('-', 10);
+            //    string lineSpam = new string('-', 10);
 
-            Console.WriteLine(lineSpam + "\nHere's my menu for bonus marks" + lineSpam + "\n\n");
+            //    Console.WriteLine(lineSpam + "\nHere's my menu for bonus marks" + lineSpam + "\n\n");
 
-            Console.WriteLine("Enter number one: ");
-            int numberOne = int.Parse(Console.ReadLine());
+            //    Console.WriteLine("Enter number one: ");
+            //    int numberOne = int.Parse(Console.ReadLine());
 
-            Console.WriteLine(lineSpam + "\nEnter which operator you'd like to use from the list below\n" + lineSpam +
-                "\n\n1 - Addition." +
-                "\n2 - Subtraction." +
-                "\n3 - Multiplication." +
-                "\n4 - Division.");
-            string answer = Console.ReadLine();
-            
-            Console.WriteLine("Enter number two: ");
-            int numberTwo = int.Parse(Console.ReadLine());
+            //    Console.WriteLine(lineSpam + "\nEnter which operator you'd like to use from the list below\n" + lineSpam +
+            //        "\n\n1 - Addition." +
+            //        "\n2 - Subtraction." +
+            //        "\n3 - Multiplication." +
+            //        "\n4 - Division.");
+            //    string answer = Console.ReadLine();
 
-            if (int.TryParse(answer, out _))
+            //    Console.WriteLine("Enter number two: ");
+            //    int numberTwo = int.Parse(Console.ReadLine());
+
+            //    if (int.TryParse(answer, out _))
+            //    {
+            //        Calculation(numberOne, numberTwo, operationInt: int.Parse(answer));
+            //    }
+            //    else if (char.TryParse(answer, out _))
+            //    {
+            //        Calculation(numberOne, numberTwo, operationChar: char.Parse(answer));
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("Literally not what I asked for, retard.");
+            //    }
+            //}
+
+            //static void Calculation(int numberOne, int numberTwo, int operationInt = -1, char operationChar = '.') {
+
+            //    (int inputInt, char inputChar) tuple = (operationInt, operationChar);
+
+            //    switch (tuple)
+            //    {
+            //        case (_, '+'):
+            //        case (1, _):
+            //            Console.WriteLine(numberOne + numberTwo);
+            //            break;
+            //        case (_, '-'):
+            //        case (2, _):
+            //            Console.WriteLine(numberOne - numberTwo);
+            //            break;
+            //        case (_, '*'):
+            //        case (3, _):
+            //            Console.WriteLine(numberOne * numberTwo);
+            //            break;
+            //        case (_, '/'):
+            //        case (4, _):
+            //            Console.WriteLine(numberOne / numberTwo);
+            //            break;
+            //        default:
+            //            Console.WriteLine("How? Literally how. ");
+            //            break;
+            //    }
+            //}
+
+            //4 - Write a C# console app that accepts a number and print its multiplication table.
+            //Sample input: 2
+            //Expected output:
+            //2x1 = 2
+            //2x2 = 4
+            //2x3 = 6...
+            //2x10...
+
+            //Console.WriteLine("Enter how many multiplications you would like: ");
+            //int numberOfMultiples = int.Parse(Console.ReadLine());
+
+            //Console.WriteLine("What is the number you'd like multiples of: ");
+            //int numberToMultiply = int.Parse(Console.ReadLine());
+            //Console.WriteLine();
+
+            //for (int i = 1; i <= numberOfMultiples; i++)
+            //{
+            //    Console.WriteLine(numberToMultiply + "x" + i + " = " + numberToMultiply * i);
+            //}
+
+            //5 - Write a C# console app that accepts a character and check if it is a vowel, a digit, or any other character.
+            //To make it easier try converting the character to lowercase before doing the checks.
+            //Sample input: a
+            //Expected output: It's a vowel.
+            //Sample input: 5
+            //Expected output: It's a digit.
+            //Sample input: t
+            //Expected output: it's another character.
+
+            //    Console.WriteLine("Enter a digit, a letter, or a vowel: ");
+            //    string inputFromUser = Console.ReadKey().KeyChar.ToString().ToLower();
+            //    Console.WriteLine();
+
+            //    if (IsDigit(inputFromUser))
+            //    {
+            //        Console.WriteLine("It's a digit.");
+            //    }
+            //    else if (IsVowel(inputFromUser))
+            //    {
+            //        Console.WriteLine("It's a vowel.");
+            //    }
+            //    else
+            //    {
+            //        Console.WriteLine("it's another character.");
+            //    }
+
+
+            //static bool IsDigit(string inputFromUser)
+            //{
+            //    if (int.TryParse(inputFromUser, out _))
+            //    {
+            //        return true;
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+
+            //static bool IsVowel(string inputFromUser)
+            //{
+            //    char[] charArray = {'a', 'e', 'i', 'o', 'u', 'y'};
+
+
+            //    for (int i = 0; i < charArray.Length; i++)
+            //    {
+            //        if (inputFromUser[0] == charArray[i])
+            //        {
+            //            return true;
+            //        }
+            //        else
+            //        {
+            //            return false;
+            //        }
+            //    }
+
+            //    if (int.TryParse(inputFromUser, out _))
+            //    {
+            //        return true; //Returning 0 because it's a digit
+            //    }
+            //    else
+            //    {
+            //        return false;
+            //    }
+            //}
+
+            //=============================================================================================
+            //    C# Exercises - Part Two
+            //=============================================================================================
+            //ATTENTION: To make it more challenging make sure your programs only accept the correct input.
+            //=============================================================================================
+            //Some other useful links:
+            //- Lists: https://docs.microsoft.com/en-us/dotnet/api/system.collections.generic.list-1?view=netframework-4.7.2
+            //- Arrays: https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/arrays/
+            //- Discussion Arrays vs Lists: https://stackoverflow.com/questions/434761/array-versus-listt-when-to-use-which
+            //=============================================================================================
+
+            //6 - Write a C# console application that accepts a letter or number and a width (number) and displays a triangle of that width, using that character.
+            //Sample input: A 
+            //Sample input width: 5 
+            //Expected output:
+            //AAAAA
+            //AAAA
+            //AAA
+            //AA
+            //A
+            //*BONUS: Display the triangle in reverse order
+            //Expected output:
+            //A
+            //AA
+            //AAA
+            //AAAA
+            //AAAAA
+
+
+            //Console.WriteLine("Enter a letter: ");
+            //char charFromUser = Console.ReadKey().KeyChar;
+            //Console.WriteLine();
+
+            //int maxWidth;
+
+            //Console.WriteLine("Enter a digit: ");
+            //while (!int.TryParse(Console.ReadLine(), out maxWidth))
+            //{
+            //    Console.ForegroundColor = ConsoleColor.Red;
+            //    Console.WriteLine("ENTER A DIGIT: ");
+            //}
+
+            //Console.ResetColor();
+            //Console.WriteLine();
+
+            //for (int i = maxWidth; i > 0; i--) //Prints descending rows
+            //{
+            //    Console.WriteLine(new string(charFromUser, i));
+            //}
+            //for (int i = 0; i <= maxWidth; i++) //Prints ascending rows
+            //{
+            //    Console.WriteLine(new string(charFromUser, i));
+            //}
+
+            //7 - Write a C# console app that accepts a grade from a student and show the description according to the table below:
+            //Grade Description
+            //A Excellent
+            //B Good
+            //C Average
+            //D Fail
+            //Sample input: C
+            //Expected output: Average
+
+            Dictionary<char, string> grades = new Dictionary<char, string>()
             {
-                Calculation(numberOne, numberTwo, operationInt: int.Parse(answer));
-            }
-            else if (char.TryParse(answer, out _))
-            {
-                Calculation(numberOne, numberTwo, operationChar: char.Parse(answer));
-            }
-            else
-            {
-                Console.WriteLine("Literally not what I asked for, retard.");
-            }
-        }
+                ['A'] = "Excellent",
+                ['B'] = "Good",
+                ['C'] = "Average",
+                ['D'] = "Fail",
+            };
 
-        static void Calculation(int numberOne, int numberTwo, int operationInt = -1, char operationChar = '.') {
+            string gradeDescription;
 
-            (int inputInt, char inputChar) tuple = (operationInt, operationChar);
 
-            switch (tuple)
+            foreach (KeyValuePair<char, string> entry in grades)
             {
-                case (_, '+'):
-                case (1, _):
-                    Console.WriteLine(numberOne + numberTwo);
-                    break;
-                case (_, '-'):
-                case (2, _):
-                    Console.WriteLine(numberOne - numberTwo);
-                    break;
-                case (_, '*'):
-                case (3, _):
-                    Console.WriteLine(numberOne * numberTwo);
-                    break;
-                case (_, '/'):
-                case (4, _):
-                    Console.WriteLine(numberOne / numberTwo);
-                    break;
-                default:
-                    Console.WriteLine("How? Literally how. ");
-                    break;
+                Console.WriteLine($"{ entry.Key } { entry.Value }");
             }
+            Console.WriteLine("Enter a character grade value: ");
+
+            while (!grades.TryGetValue(char.ToUpper(Console.ReadKey().KeyChar), out gradeDescription))
+            {
+                Console.WriteLine();
+                Console.ForegroundColor = ConsoleColor.Red;
+                foreach (KeyValuePair<char, string> entry in grades)
+                {
+                    Console.WriteLine($"{ entry.Key } { entry.Value }");
+                }
+            }
+
+            Console.ResetColor();
+            Console.WriteLine();
+            Console.WriteLine(gradeDescription);
+
+            //8 - Write a C# console app that given the array below, removes all duplicated items and returns a new array in the same sequential order as the old array (minus duplicates).
+            //Sample array: ["Student", "Developer" "Great", "Developer"]
+            //Expected output: ["Student", "Great", "Developer"]
+
+
         }
     }
 }
