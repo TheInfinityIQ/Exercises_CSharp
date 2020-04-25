646dbcf2079aa4aa3934df85919d48488009ec0b
Dawson <vulcangamelabs@gmail.com>
2020-04-25 16:56:05 -0500

    Created new project to complete OOP exercises.

---
+++ ObjectOrientatedExercises/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+//=============================================================================================
+//                                C# Exercises - OOP 1
+//=============================================================================================
+//- https://docs.microsoft.com/en-us/dotnet/csharp/programming-guide/concepts/object-oriented-programming
+//=============================================================================================
+
+//1 - Create a Car class with the following properties: car�s make, model, registration, year, and current value.
+
+//2 - Implement a constructor for the Car class that will accept parameters corresponding to each of the properties above and set them.
+
+//3 - Create a method called Drive that will print "{CarModel} - Vroom! Vroom! Vroom!" to the console.
+
+//4 - On the main method create three different cars, print its full information to the console and call the Drive method.
+//*BONUS: Override the ToString method to return a string containing the full information about the car.
+
+namespace ObjectOrientatedExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
