3c1e9cbb6f6ce267d547e4f7fe5ef98006f0c268
Dawson <vulcangamelabs@gmail.com>
2020-04-25 17:16:32 -0500

    Finishes 2/4 for OOP Classes exercises (Constructors)

--- ObjectOrientatedExercises/Car.cs
+++ ObjectOrientatedExercises/Car.cs
@@ -2,7 +2,14 @@
 using System.Collections.Generic;
 using System.Text;
 
-//1 - Create a Car class with the following properties: car's make, model, registration, year, and current value.
+//1 - Create a Car class with the following properties: car�s make, model, registration, year, and current value.
+
+//2 - Implement a constructor for the Car class that will accept parameters corresponding to each of the properties above and set them.
+
+//3 - Create a method called Drive that will print "{CarModel} - Vroom! Vroom! Vroom!" to the console.
+
+//4 - On the main method create three different cars, print its full information to the console and call the Drive method.
+//*BONUS: Override the ToString method to return a string containing the full information about the car.
 
 namespace ObjectOrientatedExercises
 {
@@ -16,5 +23,14 @@ namespace ObjectOrientatedExercises
         public int Year { get; set; }
         public decimal Value { get; set; }
 
+        // Constructors
+        public Car(string make, string model, string registration, int year, decimal value)
+        {
+            this.Make = make;
+            this.Model = model;
+            this.Registration = registration;
+            this.Year = year;
+            this.Value = value;
+        }
     }
 }
