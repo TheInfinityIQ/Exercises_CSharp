035c0013704d5a4d564a2fe72306a2d129a00b5b
Dawson <vulcangamelabs@gmail.com>
2020-04-25 18:58:10 -0500

    Define ConsoleMenu class and ConsoleMenuOption for TODO exercise.

---
+++ ToDo_Console_Exercises/MenuSystem/ConsoleMenu.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ToDo_Console_Exercises.MenuSystem
+{
+    class ConsoleMenu
+    {
+        //Title
+        //Options
+        //Options within options
+
+        public string Title { get; set; }
+        public List<ConsoleMenuOption> Option { get; set; }
+    }
+}
---
+++ ToDo_Console_Exercises/MenuSystem/ConsoleMenuOption.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ToDo_Console_Exercises.MenuSystem
+{
+    class ConsoleMenuOption
+    {
+        //Title
+        //Actions performed when option is executed
+        //Description of Option
+        //Input needed to select Option
+        public string Title { get; set; }
+        public Action Outcome { get; set; }
+        public string Define { get; set; }
+        public string Selector { get; set; }
+
+    }
+}
