acc62fca482606a3351136f1e9bebd0cd3644dc8
Dawson <vulcangamelabs@gmail.com>
2020-04-27 15:25:48 -0500

    Staging all changed and pushing through just because I hate seeing edits and nothing is broken :)

---
+++ Calculator_Solo/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+//Create a calculator that will validate input, allow the user to use the following operands (*,/,+,-)
+
+namespace Calculator_Solo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Create Console Menu
+            //Create Console Menu Option
+
+            //Create calculator class (take number, then operand, then another number)
+            //Create methods inside calculator class that will use the operand
+            //Create method to validate input then make it type CalculatorInput 
+
+            //Create Method for output
+        }
+    }
+}
